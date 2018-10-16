b62c88c5559acb975e073fbff13a3ca84c3e139a
dasha <trofimova.daria.d@gmail.com>
2018-10-16 22:24:05 +0600

    delete classes

--- src/code/formating/Main.java
+++
@@ -1,8 +0,0 @@
-package code.formating;
-
-public class Main {
-    public static void main(String[] args){
-       String str = Loader.loadString("src/files/HelloWorld.java");
-       System.out.println(Formatter.format(str));
-    }
-}
